566e02f131a98c88373b6f03b0172841acf7a278
Zetday <muhammad.rendy.07@gmail.com>
2024-11-18 08:06:15 +0800

    Pertemuan 9

---
+++ app/Repositories/Contracts/WorkshopRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface WorkshopRepositoryInterface
+{
+    public function getAllNewWorkshop();
+
+    public function find($id);
+
+    public function getPrice($workshopId);
+}
---
+++ app/Services/FrontService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\WorkshopRepositoryInterface;
+
+class FrontService
+{
+    protected $categoryRepository;
+    protected $workshopRepository;
+
+    public function construct(WorkshopRepositoryInterface $workshopRepository,
+    CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->workshopRepository = $workshopRepository;
+    }
+
+    public function getFrontPageData()
+    {
+        $categories = $this->categoryRepository->getAllCategories();
+        $newWorkshops = $this->workshopRepository->getAllNewWorkshops();
+    }
+}
